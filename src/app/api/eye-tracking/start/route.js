00830035fcda0b8432beedadb9cc26948df317e8
gxl230001 <gxl230001@utdallas.edu>
2025-10-29 17:48:23 -0500

    adjusted for gabbylaptop

--- src/app/api/eye-tracking/start/route.js
+++ src/app/api/eye-tracking/start/route.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 export async function POST() {
   return new Promise((resolve) => {
-    exec('C:\\Users\\legab\\Downloads\\combined_integration\\eye_tracking\\venv\\Scripts\\activate && python C:\\Users\\legab\\Downloads\\combined_integration\\eye_tracking\\MonitorTracking.py', (error, stdout, stderr) => {
+    exec('C:\\Users\\legab\\Downloads\\combined_integration\\adveyes\\eye_tracking\\venv\\Scripts\\activate && python C:\\Users\\legab\\Downloads\\combined_integration\\adveyes\\eye_tracking\\MonitorTracking.py', (error, stdout, stderr) => {
       if (error) {
         console.error(stderr);
         resolve(new Response(JSON.stringify({ success: false, error: stderr }), { status: 500 }));
